e6c9d26c11d277b7229241fdcb3cc0d5e6810719
Mark Lancaster <mark@marklancaster.org>
2025-09-25 09:53:10 +1000

    added som constants

---
+++ utils/constants.py
@@ -0,0 +1,3 @@
+# TGA CONSTANTS
+TRAINING_PACKAGES = ['CHC', 'FNS', 'ICT', 'PMB', 'PSP', 'MSA07', 'SIR', 'MEA', 'SIS', 'SHB', 'SFL', 'UET', 'POL', 'HLT', 'DEF', 'AHC', 'ACM', 'TLI', 'CPC', 'CPP', 'AVI', 'CUA', 'MSF', 'UEE', 'RII', 'AUR', 'SIT', 'BSB', 'AMP', 'AUM']
+BASE_URL = "https://training.gov.au/api/training"
