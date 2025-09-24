4cbecdb97cc15974303254ae0617578b3b8938b6
Mark Lancaster <mark@marklancaster.org>
2025-09-24 22:07:38 +1000

    Implement code changes to enhance functionality and improve performance

---
+++ pages/codes_with_x.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+from pymongo import MongoClient, DESCENDING
+remoteclient = MongoClient()
+collection = remoteclient.tga.data2
+
+st.set_page_config(layout="wide")
+
+st.title("codes that end with x")
+
+
+
+num_files = st.slider("How many files to get?", 10, 5000, 100)
+
+@st.cache_data
+def get_search(search_limit=100):
+    st.write("getting latest files")
+    search = collection.find({"code": {"$regex": "X"}}).sort({'current_release': DESCENDING}).limit(num_files)
+    last_files = list(search)
+    return last_files
+
+df = pd.DataFrame(get_search(search_limit=num_files))
+columns_to_keep = ["code", "title", "type", "current_release"]
+
+df_selected = df[columns_to_keep]
+
+st.write(df_selected)
+
+
+
+last_updated = collection.find().sort({'ts': DESCENDING}).limit(1)
+st.write(f"Database last updated: {last_updated.next()['ts']}")
