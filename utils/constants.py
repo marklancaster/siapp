97bc176fff84ad97f79c0c706d0a5baaaf5d52d7
Mark Lancaster <mark@marklancaster.org>
2025-09-25 11:28:02 +1000

    Add MongoDB connection string and update BASE_URL format in constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,7 @@
+import streamlit as st
+
+MONGODB_CONNECTION_STRING = f"mongodb+srv://{st.secrets['MONGODB_USERNAME']}:{st.secrets['MONGODB_PASSWORD']}@{st.secrets['MONGODB_URI']}"
+
 # TGA CONSTANTS
 TRAINING_PACKAGES = ['CHC', 'FNS', 'ICT', 'PMB', 'PSP', 'MSA07', 'SIR', 'MEA', 'SIS', 'SHB', 'SFL', 'UET', 'POL', 'HLT', 'DEF', 'AHC', 'ACM', 'TLI', 'CPC', 'CPP', 'AVI', 'CUA', 'MSF', 'UEE', 'RII', 'AUR', 'SIT', 'BSB', 'AMP', 'AUM']
-BASE_URL = "https://training.gov.au/api/training"
+BASE_URL = "https://training.gov.au/api/training/"
