70a35b13838e04f46f65720272be3a2f6a1f6093
Mark Lancaster <mark@marklancaster.org>
2025-09-25 11:28:16 +1000

    Add initial db.py file with MongoDB connection setup

---
+++ utils/db.py
@@ -0,0 +1,9 @@
+# from pymongo import AsyncMongoClient
+# import streamlit as st
+
+# connection_string = f"mongodb+srv://{st.secrets['MONGODB_USERNAME']}:{st.secrets['MONGODB_PASSWORD']}@{st.secrets['MONGODB_URI']}"
+# _connection = AsyncMongoClient(connection_string)
+# db = _connection.tga
+
+
+
